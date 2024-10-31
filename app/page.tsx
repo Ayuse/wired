4cdd216e506c7071599ea88622c864ca7f800024
Ayuse <ayodeleoluwasina@gmail.com>
2024-10-31 12:48:10 +0100

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="bg-white text-black">Hello World</div>;
+  return (
+    <div className="bg-white text-black h-full w-full flex items-center content-center text-4xl">
+      Hello World
+    </div>
+  );
 }
