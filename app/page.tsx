3b323259899c972b467f1e4e0b1eba33c443ebfe
Ayuse <ayodeleoluwasina@gmail.com>
2024-10-31 12:53:41 +0100

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-white text-black h-full w-full flex items-center content-center text-4xl">
+    <div className="bg-white text-black h-screen w-screen flex items-center justify-center text-4xl">
       Hello World
     </div>
   );
